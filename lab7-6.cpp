99f004db23ddffef4d369532b7d138acb12a3d9a
MatthewG610 <mgodoy0101@gmail.com>
2025-03-09 21:23:23 -0400

    Update lab7-6.cpp

--- lab7-6.cpp
+++ lab7-6.cpp
@@ -1,12 +1,33 @@
 //Lab7-6.cpp - calculates and displays the average price
-//Created/revised by <your name> on <current date>
+ //Created/revised by <your name> on <current date>
 
-#include <iostream>
-#include <iomanip>
-using namespace std;
+ #include <iostream>
+ #include <iomanip>
+ using namespace std;
 
-int main()
-{
+ int main()
+ {
+	int numPrice = 0; //counter
+		double price = 0.0;
+		double totalPrice = 0.0; //accumulator
+		double avgPrice = 0.0;
 
+		cout << "Price (negative number to end): ";
+		cin >> price;
+		while (price >= 0.0)
+		{
+			numPrice += 1;
+			totalPrice += price;
+			cout << "Next price: (negative number to end): ";
+			cin >> price;
+		}   
+
+		if (numPrice > 0)
+			avgPrice = totalPrice / numPrice;
+		else
+			avgPrice = 0.0;
+
+		cout << fixed << setprecision(2) << endl;
+		cout << "Average price: " << avgPrice << endl;
 	return 0;
-}	//end of main function
+ }	//end of main function
