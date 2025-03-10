a27b1c9bb6e8ba08ff50f7a37e9566580d376cb8
MatthewG610 <mgodoy0101@gmail.com>
2025-03-09 21:22:39 -0400

    Update lab7-3.cpp

--- lab7-3.cpp
+++ lab7-3.cpp
@@ -1,13 +1,28 @@
 //Lab7-3.cpp - calculates the average number of text
-//messages sent each day for 7 days
-//Created/revised by <your name> on <current date>
+ //messages sent each day for 7 days
+ //Created/revised by <your name> on <current date>
 
-#include <iostream>
-#include <iomanip>
-using namespace std;
+ #include <iostream>
+ #include <iomanip>
+ using namespace std;
 
-int main()
-{	
+ int main()
+ {	
+     int day = 1;              
+     int totalTexts = 0;
+     int dailyTexts = 0;
+     double average = 0.0;
+     const int totalDays = 7;   
 
-    return 0;
-}   //end of main function
+     while (day <= totalDays) {  
+         cout << "How many text messages did you send on day " << day << "? ";
+         cin >> dailyTexts;
+         totalTexts += dailyTexts;
+         day++;  
+     }
+
+     average = totalTexts / static_cast<double>(totalDays); 
+     cout << fixed << setprecision(0);
+     cout << endl << "You sent approximately " << average << " text messages per day." << endl;
+     return 0;
+ }   //end of main function
