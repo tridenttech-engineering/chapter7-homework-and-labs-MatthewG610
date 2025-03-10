82aaf198e54ca80950f96aa13c1928c0c5f79de5
MatthewG610 <mgodoy0101@gmail.com>
2025-03-09 21:21:17 -0400

    Update hw7-16.cpp

--- hw7-16.cpp
+++ hw7-16.cpp
@@ -1,26 +1,29 @@
 //hw7-16.cpp - displays three tip amounts
-//Created/revised by <your name> on <current date>
+ //Created/revised by <your name> on <current date>
 
-#include <iostream>
-#include <iomanip>
-using namespace std;
+ #include <iostream>
+ #include <iomanip>
+ using namespace std;
 
-int main()
-{
+ int main()
+ {
 	double bill = 0.0;
 	double tip = 0.0;
+	 double rate = 0.1;
 
 	cout << "Bill amount: ";
 	cin >> bill;
 
 	cout << fixed;
-	for (double rate = 0.1; rate <= 0.2; rate += 0.05)
+	while (rate <= 0.2)
 	{
 		tip = bill * rate;
 		cout << setprecision(0);
 		cout << rate * 100 << "% tip: ";
 		cout << setprecision(2);
 		cout << "$" << tip << endl;
+		rate += 0.05;
 	}   // end for
 	return 0;
-}	//end of main function
+ }	//end of main function
+	
